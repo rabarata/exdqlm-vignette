9eace32c584cd33177934b3f064210c7be5690fe
rabarata <raquel.a.barata@gmail.com>
2026-04-26 16:43:41 -0700

    update example 3, add example 3 BTflow/BTprec option

--- examples.R
+++ examples.R
@@ -134,7 +134,7 @@ for(i in 1:nrow(possible.dfs)){
      metrics[i,] = c(temp.check$m1.CRPS, temp.check$m1.KL)
    }
 # optimal dfs based off CRPS
-possible.dfs[which.min(crpss),]
+possible.dfs[which.min(metrics[, "CRPS"]),]
 #
 M1mcmc = exdqlmMCMC(y = sunspot.year, p0 = 0.85, model = model,
                 df = c(0.9,0.85), dim.df = c(1,8),
@@ -147,74 +147,78 @@ M2mcmc = exdqlmMCMC(y = sunspot.year, p0 = 0.85, model = model,
 
 #####################
 ##### example 3 #####
+##### OPTION 1 ######
 #####################
 #
 # figure 5 (ex3data.png)
 par(mfrow=c(2,1)) # not shown in article code chunk
-plot(log(BTflow), col="dark grey",cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
-plot(nino34, col="dark grey",cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
+plot(log(BTflow), col="dark grey",xlim = c(1988,2025),cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
+plot(nino34, col="dark grey",xlim = c(1988,2025),cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
 #
-trend.comp = polytrendMod(1,m0=3,C0=0.1)
-seas.comp = seasMod(p = 12, h = 1, C0 = diag(1,2))
+trend.comp = polytrendMod(1, m0=3, C0=0.1)
+seas.comp = seasMod(p = 12, h = c(1, 2, 0.1469118636), C0 = diag(1,6))
 model = trend.comp + seas.comp
 #
-reg.comp = regMod(nino34, m0 = 1, C0 = 1)
+reg.comp = regMod(nino34, m0 = 0, C0 = matrix(1,1,1))
 model.w.reg = model + reg.comp
 #
-possible.lams = seq(0.975,0.99,0.005)
-KLs <- vector("numeric")
-crpss <- vector("numeric")
+possible.lams = seq(0.1,0.9,0.1)
+metrics <- matrix(NA_real_, length(possible.lams), 3,
+                  dimnames = list(NULL, c("CRPS","KL","pplc")))
+ref.samp = rnorm(length(BTflow))
 for(i in 1:length(possible.lams)){
-  temp.M2 = transfn_exdqlmLDVB(y = log(BTflow), p0 = 0.15, model = model,
-                               df = c(1,0.9), dim.df = c(1,2),
+  temp.M2 = exdqlmTransferLDVB(y = log(BTflow), p0 = 0.15, model = model,
+                               df = c(0.97,0.97), dim.df = c(1,6),
                                X = nino34, tf.df = c(0.95), lam = possible.lams[i],
                                tf.m0 = c(0,0), tf.C0 = diag(c(0.1,0.005),2),
-                               fix.sigma = FALSE,gam.init = -0.1,
+                               sig.init = 0.1, gam.init = -0.1,
                                tol = 0.05, verbose = FALSE)
-  temp.d = exdqlmDiagnostics(temp.M2, plot = FALSE)
-  KLs = c(KLs,temp.d$m1.KL)
-  crpss = c(crpss,temp.d$m1.CRPS)
+  temp.check = exdqlmDiagnostics(temp.M2, plot = FALSE, ref = ref.samp)
+  metrics[i,] = c(temp.check$m1.CRPS, temp.check$m1.KL,temp.check$m1.pplc)
 }
+# not shown in article code chunk
+par(mfrow=c(3,1)) 
+plot(possible.lams,metrics[,1],type="l",main="CRPS")
+plot(possible.lams,metrics[,2],type="l",main="KLs")
+plot(possible.lams,metrics[,3],type="l",main="pplcs")
 # optimal lam based off KL divergence
-plot(possible.lams,crpss,type="l")
-plot(possible.lams,KLs,type="l")
-possible.lams[which.min(KLs)]
+possible.lams[which.min(metrics[, "CRPS"])]
 #
 M1 = exdqlmLDVB(y = log(BTflow), p0 = 0.15, model = model.w.reg,
-                df = c(1,0.9,0.95), dim.df = c(1,2,1),
-                fix.sigma = FALSE,
+                df = c(0.97,0.97,0.97), dim.df = c(1,6,1),
+                sig.init = 0.1, gam.init = -0.1,
                 tol = 0.05, verbose = FALSE)
-M2 = transfn_exdqlmLDVB(y = log(BTflow), p0 = 0.15, model = model,
-                        df = c(1,0.9), dim.df = c(1,2),
-                        X = nino34, tf.df = c(0.95), lam = 0.985,
+M2 = exdqlmTransferLDVB(y = log(BTflow), p0 = 0.15, model = model,
+                        df = c(0.97,0.97), dim.df = c(1,6),
+                        X = nino34, tf.df = c(0.97), lam = 0.9,
                         tf.m0 = c(0,0), tf.C0 = diag(c(0.1,0.005),2),
-                        fix.sigma = FALSE,
+                        sig.init = 0.1, gam.init = -0.1,
                         tol = 0.05, verbose = FALSE)
 #
 # figure 6 (ex3quantcomps.png)
 par(mfrow=c(3,1),mar=c(4.1,4.2,1.3,1.2)) # not shown in article code chunk
-plot(log(BTflow), col = "grey", ylim = c(1,8), xlim=c(1970,1990),
+plot(log(BTflow), col = "grey", ylim = c(1,8), xlim=c(1990,2010),
                ylab = "quantile 95% CrIs")
 exdqlmPlot(M1, add = TRUE)
 exdqlmPlot(M2, add = TRUE, col = "forest green")
 #
-plot(NA, col = "grey", ylim = c(-1.5,1.5), xlim=c(1970,1990), ylab = "seasonal components")
-compPlot(M1, index = c(2,3), add = TRUE)
-compPlot(M2, index = c(2,3), add = TRUE,
+plot(NA, col = "grey", ylim = c(-2,2), xlim=c(1990,2010), ylab = "seasonal components")
+compPlot(M1, index = 2:7, add = TRUE)
+compPlot(M2, index = 2:7, add = TRUE,
          col = "forest green")
 #
-plot(NA, col = "grey", ylim = c(-0.5,1.5), xlim=c(1970,1990), ylab = "Niño 3.4 components")
-compPlot(M1, index = c(4), add = TRUE)
-compPlot(M2, index = c(4,5), add =TRUE, col = "forest green")
+plot(NA, col = "grey", ylim = c(-1.5,1.5), xlim=c(1990,2010), ylab = "Niño 3.4 components")
+compPlot(M1, index = 8, add = TRUE)
+compPlot(M2, index = 8:9, add =TRUE, col = "forest green")
 abline(h = 0, col = "orange", lty = 3, lwd = 2)
 #
 # figure 7 (ex3zetapsi.png)
 par(mfrow=c(2,1),mar=c(4.1,4.2,1.3,1.2)) # not shown in article code chunk
-compPlot(M2, index = 4, col= "forest green",
+compPlot(M2, index = 8, col= "forest green",
          add = FALSE, just.theta = TRUE)
 abline(h = 0, col = "orange", lty = 3, lwd = 2)
 title(expression(zeta[t]))
-compPlot(M2, index = 5, col = "forest green",
+compPlot(M2, index = 9, col = "forest green",
          add = FALSE, just.theta = TRUE)
 abline(h = 0, col = "orange", lty = 3, lwd = 2)
 title(expression(psi[t]))
@@ -229,4 +233,95 @@ exdqlmForecast(start.t = length(BTflow) - 18, k = 18, M2, add = TRUE, cols = c("
 checks.out = exdqlmDiagnostics(M1, M2,
                           col = c("purple","forest green"), plot = FALSE)
 print(checks.out)
+
+
+#####################
+##### example 3 #####
+##### OPTION 2 ######
+#####################
+
+# figure 5 (ex3data.png)
+par(mfrow=c(2,1)) # not shown in article code chunk
+plot(BTflow, col="dark grey",cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
+plot(BTprec, col="dark grey",cex.lab=1.5,cex.axis=1.5,cex.main=1.8) # not shown in article code chunk
+#
+trend.comp = polytrendMod(1,m0=3,C0=0.1)
+seas.comp = seasMod(p = 12, h = c(1, 2, 0.1469118636), C0 = diag(1,6))
+model = trend.comp + seas.comp
+#
+reg.comp = regMod(BTprec, m0 = 0, C0 = 1)
+model.w.reg = model + reg.comp
+#
+possible.lams = seq(0.1,0.9,0.1)
+metrics <- matrix(NA_real_, length(possible.lams), 3,
+                  dimnames = list(NULL, c("CRPS","KL","pplc")))
+ref.samp = rnorm(length(BTflow))
+for(i in 3:length(possible.lams)){
+  temp.M2 = exdqlmTransferLDVB(y = BTflow, p0 = 0.15, model = model,
+                               df = c(0.97,0.97), dim.df = c(1,6),
+                               X = BTprec, tf.df = c(0.95), lam = possible.lams[i],
+                               tf.m0 = c(0,0), tf.C0 = diag(c(0.1,0.005),2),
+                               sig.init = 0.1, gam.init = -0.1,
+                               tol = 0.05, verbose = FALSE)
+  temp.check = exdqlmDiagnostics(temp.M2, plot = FALSE, ref = ref.samp)
+  metrics[i,] = c(temp.check$m1.CRPS, temp.check$m1.KL,temp.check$m1.pplc)
+}
+# not shown in article code chunk
+par(mfrow=c(3,1)) 
+plot(possible.lams,metrics[,1],type="l",main="CRPS")
+plot(possible.lams,metrics[,2],type="l",main="KLs")
+plot(possible.lams,metrics[,3],type="l",main="pplcs")
+# optimal lam based off KL divergence
+possible.lams[which.min(metrics[, "CRPS"])] # 0.5
+#
+M1 = exdqlmLDVB(y = BTflow, p0 = 0.15, model = model.w.reg,
+                df = c(0.97,0.97,0.97), dim.df = c(1,6,1),
+                sig.init = 0.1, gam.init = -0.1,
+                tol = 0.05, verbose = FALSE)
+M2 = exdqlmTransferLDVB(y = BTflow, p0 = 0.15, model = model,
+                        df = c(0.97,0.97), dim.df = c(1,6),
+                        X = BTprec, tf.df = c(0.97), lam = 0.5,
+                        tf.m0 = c(0,0), tf.C0 = diag(c(0.1,0.005),2),
+                        sig.init = 0.1, gam.init = -0.1,
+                        tol = 0.05, verbose = FALSE)
+#
+# figure 6 (ex3quantcomps.png)
+par(mfrow=c(3,1),mar=c(4.1,4.2,1.3,1.2)) # not shown in article code chunk
+plot(BTflow, col = "grey",ylim = c(0,500), xlim=c(1992,2008),
+     ylab = "quantile 95% CrIs")
+exdqlmPlot(M1, add = TRUE)
+exdqlmPlot(M2, add = TRUE, col = "forest green")
+#
+plot(NA, col = "grey", ylim = c(-58,58), xlim=c(1992,2008), ylab = "seasonal components")
+compPlot(M1, index = 2:7, add = TRUE)
+compPlot(M2, index = 2:7, add = TRUE,
+         col = "forest green")
+#
+plot(NA, col = "grey", ylim = c(0,300), xlim=c(1992,2008), ylab = "Niño 3.4 components")
+compPlot(M1, index = 8, add = TRUE)
+compPlot(M2, index = 8:9, add =TRUE, col = "forest green")
+abline(h = 0, col = "orange", lty = 3, lwd = 2)
+#
+# figure 7 (ex3zetapsi.png)
+par(mfrow=c(2,1),mar=c(4.1,4.2,1.3,1.2)) # not shown in article code chunk
+compPlot(M2, index = 8, col= "forest green",
+         add = FALSE, just.theta = TRUE)
+abline(h = 0, col = "orange", lty = 3, lwd = 2)
+title(expression(zeta[t]))
+compPlot(M2, index = 9, col = "forest green",
+         add = FALSE, just.theta = TRUE)
+abline(h = 0, col = "orange", lty = 3, lwd = 2)
+title(expression(psi[t]))
+#
+M2$median.kt
+#
+# figure 8 (ex3forecast.png)
+par(mfrow=c(1,1),mar=c(4.1,4.2,1.3,1.2)) # not shown in article code chunk
+exdqlmForecast(start.t = length(BTflow) - 18, k = 18, M1)
+exdqlmForecast(start.t = length(BTflow) - 18, k = 18, M2, add = TRUE, cols = c("forest green","green"))
+#
+checks.out = exdqlmDiagnostics(M1, M2,
+                               col = c("purple","forest green"), plot = FALSE)
+print(checks.out)
+
 
